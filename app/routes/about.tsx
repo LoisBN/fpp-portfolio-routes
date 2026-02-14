559c2fa11c444ddcdc4e5ea9caa786b962b375b3
LoisBN <47926874+LoisBN@users.noreply.github.com>
2026-02-14 23:33:42 +0700

    Add starter code for exercise

---
+++ app/routes/about.tsx
@@ -0,0 +1,15 @@
+// TODO: Create an About page component
+// This should tell visitors about you!
+
+export default function About() {
+  return (
+    <div className="space-y-8">
+      <div className="bg-gray-800 rounded-lg p-8 border border-gray-700">
+        <h2 className="text-3xl font-bold text-white mb-4">About Me</h2>
+        <p className="text-gray-300 text-lg leading-relaxed">
+          TODO: Write your bio here
+        </p>
+      </div>
+    </div>
+  );
+}
